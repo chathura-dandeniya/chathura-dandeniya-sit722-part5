4ef95a9dbd51b9b54ffb418be7dc88b5b7e63224
chathura-dandeniya <s222489357@deakin.edu.au>
2024-09-27 14:49:28 +1000

    Modify the deploy.yaml file

--- container-registry.tf
+++ container-registry.tf
@@ -8,3 +8,16 @@ resource "azurerm_container_registry" "container_registry" {
   admin_enabled       = true
   sku                 = "Basic"
 }
+
+output "registry_username" {
+  value = azurerm_container_registry.container_registry.admin_username
+}
+
+output "registry_password" {
+  value = azurerm_container_registry.container_registry.admin_password
+}
+
+output "registry_login_server" {
+  value = azurerm_container_registry.container_registry.login_server
+}
+
