55a8aa78e9e8870649caf775c2d15dc37af5b1f0
chathura-dandeniya <s222489357@deakin.edu.au>
2024-09-28 12:18:21 +1000

    Added Database URLs

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://librarypart05_jhl4_user:wFnl9yYr71IwSXnwHMkh1QrFux1cWYit@dpg-crrm4i56l47c73cl6e4g-a.oregon-postgres.render.com/librarypart05_jhl4')
